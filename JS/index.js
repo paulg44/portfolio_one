77281b71f0d85bfcc2da25f390ff5ab7e5d8bd91
paulg44 <paulgarton1984@hotmail.com>
2023-05-17 20:50:12 +0100

    revert project page for now

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+// Javascript for Text Scroll on Homepage
+/*
+   Plan 
+   1 Make variable for scroll text 
+   2 Make array with different texts to scroll 
+   3 Loop through the array 
+   4 Set textContent to the array
+*/
+
+// Variables
+const scrollText = document.querySelector('.text-scroll');
+const textArray = ['Software Developer', 'Web Developer', 'Web Designer'];
+
+// Function
+function scrollTextFunction() {
+  let i = 0;
+  setInterval(function () {
+    scrollText.textContent = textArray[i];
+    i++;
+    if (i > textArray.length - 1) {
+      i = 0;
+    }
+  }, 2900);
+}
+scrollTextFunction();
