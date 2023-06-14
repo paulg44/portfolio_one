7ec5c9fbd34d8e1d0d1d200760a0fbe64a3264dd
paulg44 <paulgarton1984@hotmail.com>
2023-06-14 21:17:49 +0100

    add more info to projects page

--- tests/portfolio.test.js
+++ tests/portfolio.test.js
@@ -3,6 +3,8 @@ const { test, expect } = require('@playwright/test');
 
 // Global Variables
 const homePage = 'http://127.0.0.1:5500/portfolio_one/';
+const projectsPage = 'http://127.0.0.1:5500/portfolio_one/projects.html';
+const contactPage = 'http://127.0.0.1:5500/portfolio_one/contact.html';
 
 // Title Test
 test('page title', async ({ page }) => {
@@ -11,3 +13,6 @@ test('page title', async ({ page }) => {
 
   await expect(page).toHaveTitle(pageTitle);
 });
+
+// Page Navigation Tests
+test('project page from homepage', async ({ page }) => {});
