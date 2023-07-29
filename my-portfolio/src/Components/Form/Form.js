62b4473f1d57990f0b119c26cf03c3e271cd6237
paulg44 <paulgarton1984@hotmail.com>
2023-07-29 21:53:24 +0100

    get netlify form to work

---
+++ my-portfolio/src/Components/Form/Form.js
@@ -0,0 +1,24 @@
+// Contact From Component
+import './Form.css';
+
+function ContactForm() {
+  return (
+    <form name="contact" class="form" netlify>
+      <input type="hidden" name="contact" value="contact" />
+      <div class="left-form">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" required />
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" required />
+        <label for="phone">Phone No</label>
+        <input type="number" id="phone" name="phone" />
+      </div>
+      <div class="right-form">
+        <textarea name="text" placeholder="Anything to add..."></textarea>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
+}
+
+export default ContactForm;
--- my-portfolio/src/Pages/Contact/Contact.js
+++ my-portfolio/src/Pages/Contact/Contact.js
@@ -1,30 +1,17 @@
 // Component for Contact Page
 
-import "./Contact.css"
+import './Contact.css';
+import ContactForm from '../../Components/Form/Form';
 
 function Contact() {
-    return(   
+  return (
     <section id="contact" class="contact">
-    
-    {/* <!-- Contact Form --> */}
-       <div class="contact-form">
-         <form name="contact" class="form" netlify>
-           <div class="left-form">
-             <label for="name">Name</label>
-             <input type="text" id="name" name="name" required />
-             <label for="email">Email</label>
-             <input type="text" id="email" name="email" required />
-             <label for="phone">Phone No</label>
-             <input type="number" id="phone" name="phone" />
-           </div>
-           <div class="right-form">
-             <textarea name="text" placeholder="Anything to add..."></textarea>
-             <button type="submit">Submit</button>
-            </div>
-         </form>
-        </div>
+      {/* <!-- Contact Form --> */}
+      <div class="contact-form">
+        <ContactForm />
+      </div>
     </section>
-    )
+  );
 }
 
-export default Contact
+export default Contact;
