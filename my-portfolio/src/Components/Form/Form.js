77bb7acb7d421efaf446e70d530d58a29a488842
paulg44 <paulgarton1984@hotmail.com>
2023-07-29 22:08:45 +0100

    last try at netlify form for today

--- my-portfolio/src/Components/Form/Form.js
+++ my-portfolio/src/Components/Form/Form.js
@@ -1,20 +1,51 @@
 // Contact From Component
 import './Form.css';
+import React, { useState } from 'react';
 
 function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [text, setText] = useState('');
+
   return (
     <form name="contact" class="form" netlify>
       <input type="hidden" name="form-name" value="contact" />
       <div class="left-form">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" required />
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email" required />
-        <label for="phone">Phone No</label>
-        <input type="number" id="phone" name="phone" />
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          required
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          required
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <label htmlFor="phone">Phone No</label>
+        <input
+          type="number"
+          id="phone"
+          name="phone"
+          value={phone}
+          onChange={e => setPhone(e.target.value)}
+        />
       </div>
       <div class="right-form">
-        <textarea name="text" placeholder="Anything to add..."></textarea>
+        <textarea
+          name="text"
+          placeholder="Anything to add..."
+          value={text}
+          onChange={e => setText(e.target.value)}
+        ></textarea>
         <button type="submit">Submit</button>
       </div>
     </form>
