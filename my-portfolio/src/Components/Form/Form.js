d48e7d91ed64333bf18419852ea1bb7fb49e54de
paulg44 <paulgarton1984@hotmail.com>
2023-07-30 12:49:14 +0100

    2nd attempt at netlify form

--- my-portfolio/src/Components/Form/Form.js
+++ my-portfolio/src/Components/Form/Form.js
@@ -1,51 +1,20 @@
 // Contact From Component
 import './Form.css';
-import React, { useState } from 'react';
 
 function ContactForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [text, setText] = useState('');
-
   return (
-    <form name="contact" class="form" netlify>
+    <form method="POST" name="contact" class="form" netlify>
       <input type="hidden" name="form-name" value="contact" />
       <div class="left-form">
         <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
+        <input type="text" id="name" name="name" required />
         <label htmlFor="email">Email</label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          required
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
+        <input type="text" id="email" name="email" required />
         <label htmlFor="phone">Phone No</label>
-        <input
-          type="number"
-          id="phone"
-          name="phone"
-          value={phone}
-          onChange={e => setPhone(e.target.value)}
-        />
+        <input type="number" id="phone" name="phone" />
       </div>
       <div class="right-form">
-        <textarea
-          name="text"
-          placeholder="Anything to add..."
-          value={text}
-          onChange={e => setText(e.target.value)}
-        ></textarea>
+        <textarea name="text" placeholder="Anything to add..."></textarea>
         <button type="submit">Submit</button>
       </div>
     </form>
