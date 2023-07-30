6daca1cabb86b452682e652fef5696bd1301692b
paulg44 <paulgarton1984@hotmail.com>
2023-07-30 13:28:28 +0100

    finish submission page & media queries

--- my-portfolio/src/Pages/Submission/Submission.js
+++ my-portfolio/src/Pages/Submission/Submission.js
@@ -1,10 +1,16 @@
 // Component for successful submission
-import { Link } from 'react-router-dom';
+import './Submission.css';
+import smiley from '../../IMG/vecteezy_3d-render-emoticon-or-emoji-perfect-for-sosial-media_10196114_814-removebg-preview.jpg';
 
 function Submission() {
   return (
-    <div>
-      <h2>Thanks! Test</h2>
+    <div className="submission">
+      <h2>Thank you for your submission!</h2>
+      <p>
+        I will be in touch in due course. Thank you for taking the time to look
+        through my portfolio, and lastly, have a nice day!
+      </p>
+      <img src={smiley} alt="siley emoji faces" />
     </div>
   );
 }
