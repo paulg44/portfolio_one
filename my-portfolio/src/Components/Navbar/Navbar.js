717afb814787d88bcba638c96219655fa193a9db
paulg44 <paulgarton1984@hotmail.com>
2024-04-06 19:37:48 +0100

    add project link to navbar

--- my-portfolio/src/Components/Navbar/Navbar.js
+++ my-portfolio/src/Components/Navbar/Navbar.js
@@ -1,19 +1,27 @@
 // Component for Navbar
-import "./Navbar.css"
-import {Link} from "react-router-dom"
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-    return (
+  return (
     //  Navbar
     <nav id="navbar" className="navbar">
       <div class="nav-links">
-        <Link to={"/"} id="logo" className="logo"
-          ><span>&lt;</span>Home<span>/&gt;</span></Link>
+        <Link to={'/'} id="logo" className="logo">
+          <span>&lt;</span>Home<span>/&gt;</span>
+        </Link>
         {/* <a href="blogpost.html" className="contact-button">Blogpost</a> */}
-        <Link to={"/contact"} className="contact-button">Contact</Link>
+        <div className="navbarLeftLinks">
+          <Link to={'/projects'} className="contact-button">
+            Projects
+          </Link>
+          <Link to={'/contact'} className="contact-button">
+            Contact
+          </Link>
+        </div>
       </div>
     </nav>
-    )
+  );
 }
 
-export default Navbar
+export default Navbar;
