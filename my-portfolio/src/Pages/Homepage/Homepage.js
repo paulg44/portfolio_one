48aa15d6c472bfbf69fea380d742871383046292
paulg44 <paulgarton1984@hotmail.com>
2024-04-06 19:33:18 +0100

    change projects layout, homepage about and font

--- my-portfolio/src/Pages/Homepage/Homepage.js
+++ my-portfolio/src/Pages/Homepage/Homepage.js
@@ -31,28 +31,21 @@ function Homepage() {
       {/* <!-- Main Header --> */}
       <main className="main-container">
         <div className="main-header">
-          <h1>Hey, I'm Paul</h1>
-          <p>An Aspiring</p>
+          {/* Change to morning, afternoon or evening dependant on local time */}
+          <h1>Hello, I'm Paul</h1>
+          <p>A creative</p>
           <p className="text-scroll">{textArray[currentIndex]}</p>
         </div>
         {/* <!-- About me --> */}
         <div className="about">
           <h3>About</h3>
           <p>
-            Hello, and welcome to my portfolio. I'm a former painter and
-            decorator turned junior developer, looking to make a change in my
-            life and in others. I am a graduate of the
-            <code>
-              <a
-                href="https://www.schoolofcode.co.uk/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                &nbsp;School of Code&nbsp;
-              </a>
-            </code>
-            bootcamp, and have a passion for creativity, problem solving and all
-            things tech related.
+            I am enthusiastic about pursuing a career as a junior developer,
+            where I can make a real difference and impact in my work. As I grow
+            and become more experienced, I am looking to gain a deeper knowledge
+            in software development and enhance my ability to add value within
+            the tech industry. I have a passion for creativity, problem solving
+            and all things tech related.
             <br />
             <br />
             If you would like to get in touch, please contact me
