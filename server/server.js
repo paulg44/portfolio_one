4cc57dd5e98d675914a95d5a387118186682f3b9
paulg44 <paulgarton1984@hotmail.com>
2024-04-07 14:31:30 +0100

    add server and dependencies. Set up basic server (for blogs)

---
+++ server/server.js
@@ -0,0 +1,28 @@
+// Server file
+
+import express from "express";
+import pkg from "pg";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+const app = express();
+
+const port = process.env.REACT_APP_PORT;
+const connectionString = process.env.REACT_APP_BLOG_DB_STRING;
+
+export const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
