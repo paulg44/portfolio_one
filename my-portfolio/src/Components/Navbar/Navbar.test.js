fd8f6219f3a8086cd7c46a1886184933eb98d8cc
paulg44 <paulgarton1984@hotmail.com>
2024-04-08 20:38:45 +0100

    add last two tests to navbar links

--- my-portfolio/src/Components/Navbar/Navbar.test.js
+++ my-portfolio/src/Components/Navbar/Navbar.test.js
@@ -4,6 +4,8 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import Navbar from "./Navbar.js";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 import Projects from "../../Pages/Projects/Projects";
+import Contact from "../../Pages/Contact/Contact.js";
+import Homepage from "../../Pages/Homepage/Homepage.js";
 
 describe("navbar tests", () => {
   test("navbar renders", () => {
@@ -33,5 +35,43 @@ describe("navbar tests", () => {
     const projectsHeader = screen.getByRole("heading", { name: "My Projects" });
 
     expect(projectsHeader).toBeInTheDocument();
+  });
+
+  test("link to contact", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Navbar />
+        <Routes>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const contactsLinkBtn = screen.getByRole("link", { name: "Contact" });
+
+    fireEvent.click(contactsLinkBtn);
+
+    const contactInput = screen.getByRole("textbox", { name: "Name" });
+
+    expect(contactInput).toBeInTheDocument();
+  });
+
+  test("link to home, start on contact page", () => {
+    render(
+      <MemoryRouter initialEntries={["/contact"]}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const homeLinkBtn = screen.getByRole("link", { name: "< Home />" });
+
+    fireEvent.click(homeLinkBtn);
+
+    const homeHeader = screen.getByRole("heading", { name: "Hello, I'm Paul" });
+
+    expect(homeHeader).toBeInTheDocument();
   });
 });
