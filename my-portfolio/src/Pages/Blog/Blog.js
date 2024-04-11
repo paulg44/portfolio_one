e77e59443abb40caab3e467c67b89a4ba6bf4d45
paulg44 <paulgarton1984@hotmail.com>
2024-04-11 20:38:34 +0100

    display blogs on page and convert the date to british style date

--- my-portfolio/src/Pages/Blog/Blog.js
+++ my-portfolio/src/Pages/Blog/Blog.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 function Blogs() {
   const [displayBlog, setDisplayBlog] = useState([]);
 
+  //   Function to fetch blogs from database
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
@@ -13,17 +14,47 @@ function Blogs() {
           return blogResponse.json();
         });
         console.log(blogResponse);
-        setDisplayBlog(blogResponse);
+
+        // Format the date for each blog item
+        const blogsWithFormattedDate = blogResponse.map((blog) => {
+          const dateFromDatabase = new Date(blog.date);
+          const year = dateFromDatabase.getFullYear();
+          const month = dateFromDatabase.getMonth() + 1;
+          const day = dateFromDatabase.getDate();
+
+          const englishDateStyle = `${day < 10 ? "0" : ""}${day}-${
+            month < 10 ? "0" : ""
+          }${month}-${year}`;
+
+          return { ...blog, englishDateStyle }; // Add formatted date to blog object
+        });
+        setDisplayBlog(blogsWithFormattedDate);
       } catch (error) {
         console.error("Error fetching blog", error);
       }
     };
     fetchBlogs();
   }, []);
+
   return (
     <div className="blogs">
-      <h2>Blogs</h2>
-      <p>Test</p>
+      <h2>Welcome to my Blog</h2>
+      <div className="blogContainer">
+        {Array.isArray(displayBlog) &&
+          displayBlog.map((blog, index) => (
+            <div key={index} className="blogItem">
+              <h3>{blog.headline}</h3>
+              <div className="blogDetails">
+                <h6>{blog.author}</h6>
+                <p>{blog.englishDateStyle}</p>
+              </div>
+              <p>{blog.body}</p>
+              <a href={blog.blogLink} target="_blank" rel="noreferrer">
+                {blog.blogLink}
+              </a>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
