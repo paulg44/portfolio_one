4caeaf58d473806489c01a22100dc5f4d140437f
Paul Garton <paulgarton1984@hotmail.com>
2024-04-15 18:24:59 +0100

    balnked out blogs, again, new computer, getting to grips

--- my-portfolio/src/Pages/Blog/Blog.js
+++ my-portfolio/src/Pages/Blog/Blog.js
@@ -10,7 +10,7 @@ function Blogs() {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const blogResponse = await fetch("/api/blogs").then(
+        const blogResponse = await fetch("api.blogs").then(
           (blogResponse) => {
             return blogResponse.json();
           }
