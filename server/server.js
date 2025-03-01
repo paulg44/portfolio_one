6cc6959d316e7abaab3b078bb03a4b3b1cc602c8
Paul Garton <paulgarton1984@hotmail.com>
2025-03-01 21:45:56 +0000

    add cross origin to server to allow requests from my site

--- server/server.js
+++ server/server.js
@@ -33,9 +33,26 @@ const limiter = rateLimit({
 });
 
 app.use(limiter);
-app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: `${process.env.REACT_APP_FRONTEND_URL_PROD}`,
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://paulgarton.co.uk");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.options("*", cors());
+app.use(express.json());
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 
