b5a332d71123547162f4c063159e779faa9ac819
Paul Garton <paulgarton1984@hotmail.com>
2025-03-08 21:11:28 +0000

    pad out and add more to the json file about me: aslo tweak the prompt to be a bit more snappy in responses and remove some data

--- server/server.js
+++ server/server.js
@@ -38,35 +38,9 @@ app.use(express.json());
 
 const port = process.env.REACT_APP_PORT;
 
-// const portfolioData = JSON.parse(
-//   fs.readFileSync("./portfolioData.json", "utf8")
-// );
-
-const portfolioData = {
-  name: "Paul",
-  location: "Derby",
-  age: 42,
-  family: "Wife and two children (7 + 5)",
-  bio: "I am a software developer based in Derby",
-  goals: "I am looking for my first role within the tech community",
-  projects: [
-    {
-      name: "Shardlow St James FC",
-      description:
-        "A complete website for a local amateur football club. It uses the FA API to integrate the 4 teams fixtures, results and table data. Has a web scraper built using Playwright to scrape data from Instagram.",
-      techStack:
-        "React, Node.js, Express, Framer Motion, Nock, React Testing Library, React Router, React Spring, Playwright, FS-Extra, node-cron, simple-git",
-      website: "https://ssjfc.co.uk/",
-    },
-    {
-      name: "AMH Decorating",
-      description:
-        "A complete website for a local painter and decorator to be more visual and generate more custom. Built from scratch using Figma and designed in collaboration with the client",
-      techStack: "React, React Testing Library, React Router",
-      website: "https://amhdecorating.co.uk/",
-    },
-  ],
-};
+const portfolioData = JSON.parse(
+  fs.readFileSync("./portfolioData.json", "utf8")
+);
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 const openai = new OpenAI({ apiKey });
@@ -85,7 +59,7 @@ app.post("/user-question", async (req, res) => {
         {
           role: "developer",
 
-          content: `You are an assistant that answers questions in the style of a local from Derby, UK. You can ONLY answer questions about: - Paul's portfolio and projects - Paul himself - The city of Derby (history, culture, landmarks, sports. If a question is unrelated say: "Sorry I can only chat about Paul or Derby, my duck!" Add some local dialect and humour. Here is some information about Paul and his projects: ${JSON.stringify(
+          content: `You are an assistant that answers questions in the style of a local from Derby, UK. You can ONLY answer questions about: - Paul's portfolio and projects - Paul himself - The city of Derby (history, culture, landmarks, sports. If a question is unrelated say: "Sorry I can only chat about Paul or Derby, my duck!" Add some local dialect and humour. Try and keep the responses fairly short and snappy. Also, don't show the website addresses in the message, they are there for you to do some more research should you wish. Here is some information about Paul and his projects: ${JSON.stringify(
             portfolioData
           )}`,
         },
